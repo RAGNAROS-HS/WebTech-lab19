e76597a15481a0018d7c6fdab75036eccfbfb986
RAGNAROS-HS <hugo.s.katzer@gmail.com>
2023-01-20 13:10:24 +0100

    update button attempt

--- form_submit.js
+++ form_submit.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     
-    fetch("https://wt.ops.labs.vu.nl/api23/98ce034a",{})
+    fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{})
     .then(function(response){
         return response.json();
     })
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         var tagsInput = document.getElementById('tags').value
         var descriptionInput = document.getElementById('description').value
 
-        fetch("https://wt.ops.labs.vu.nl/api23/98ce034a", {
+        fetch("https://wt.ops.labs.vu.nl/api23/99a18706", {
             method: 'POST',
             headers:{
                 "Content-Type": "application/json; charset=UTF-8"
@@ -57,19 +57,40 @@ window.addEventListener('DOMContentLoaded', (event) => {
         reset.addEventListener("click", resetFunction);
 
         function resetFunction() {
-            fetch("https://wt.ops.labs.vu.nl/api23/98ce034a/reset", {
+
+            fetch("https://wt.ops.labs.vu.nl/api23/99a18706/reset", {
                 method: 'GET',
             })
+            .then(response=>response.json())
+            .then(data=>{ console.log(data); })
         }
 
 
         let update = document.getElementsByTagName("button")[1];
         update.addEventListener("click", updateFunction);
+        var table = document.getElementById("table"),rIndex;
 
+            
+        for(var i = 1; i < table.rows.length - 2; i++){
+            table.rows[i].addEventListener("click",function(){
+                rIndex = this.rowIndex;
+                console.log(rIndex);
+                document.getElementById("image").value = table.rows[i].cells[1].innerHTML;
+                document.getElementById("author").value = table.rows[i].cells[2].innerHTML;
+                document.getElementById("alt").value = table.rows[i].cells[3].innerHTML;
+                document.getElementById("tags").value = table.rows[i].cells[4].innerHTML;
+                document.getElementById("description").value = table.rows[i].cells[5].innerHTML;
+            })
+            console.log("listener added");
+        }                 
+                    
         function updateFunction(){
-
+            table.rows[rIndex].cells[1].innerHTML = document.getElementById("image").value;
+            table.rows[rIndex].cells[2].innerHTML = document.getElementById("author").value;
+            table.rows[rIndex].cells[3].innerHTML = document.getElementById("alt").value;
+            table.rows[rIndex].cells[4].innerHTML = document.getElementById("tags").value;
+            table.rows[rIndex].cells[5].innerHTML = document.getElementById("description").value;
         }
-
 });
 
 
