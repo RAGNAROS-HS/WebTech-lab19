5418a1f2ecbd3e80fb1608b9ecb38b46e8a4276c
RAGNAROS-HS <hugo.s.katzer@gmail.com>
2023-01-23 17:32:22 +0100

    indentation and bugfixes

--- form_submit.js
+++ form_submit.js
@@ -1,8 +1,10 @@
+/*For the filterable gallery functionality we will be using a modified implementation of the search bar from this site:
+ https://www.w3schools.com/howto/howto_js_filter_table.asp */
+
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
-
     let idclear = document.getElementById("id");
-    idclear.innerHTML = '';
+    idclear.innerHTML = '<option value="please select">Please Select</option>';
     var nameButtons = [];
     var authors = [];
     let filterbar = document.getElementById("filterBar");
@@ -37,8 +39,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
             let rowsNB = document.querySelectorAll('tr');
             let lastRow = rowsNB[rowsNB.length - 2];
             lastRow.insertAdjacentHTML('afterend', markup);
-        },
-        );
+        })
     })
     .then(function(){
         nameButtons = document.getElementsByClassName("nameButton");
@@ -65,7 +66,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 }
                 who = document.getElementById("whoFilter");
                 who.innerText = "Now filtering by: "+filter;
-            });
+            })
         }
     })
     .then(function(){
@@ -78,19 +79,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     })
 
-
     var form = document.getElementById('form');
 
-    form.addEventListener('submit', function(e) {
-        
+    form.addEventListener('submit', function(e){
         e.preventDefault()
-        var imageInput = document.getElementById('image').value
-        var authorInput = document.getElementById('author').value
-        var altInput = document.getElementById('alt').value
-        var tagsInput = document.getElementById('tags').value
-        var descriptionInput = document.getElementById('description').value
+        var imageInput = document.getElementById('image').value;
+        var authorInput = document.getElementById('author').value;
+        var altInput = document.getElementById('alt').value;
+        var tagsInput = document.getElementById('tags').value;
+        var descriptionInput = document.getElementById('description').value;
 
-        fetch("https://wt.ops.labs.vu.nl/api23/99a18706", {
+        fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{
             method: 'POST',
             headers:{
                 "Content-Type": "application/json; charset=UTF-8"
@@ -145,86 +144,86 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 }
                 who = document.getElementById("whoFilter");
                 who.innerText = "Now filtering by: "+filter;
-            });
+            })
         })
 
-        });
+    });
 
-        let reset = document.getElementsByClassName("resetButton")[0];
-        reset.addEventListener("click", resetFunction);
+    let reset = document.getElementsByClassName("resetButton")[0];
+    reset.addEventListener("click", resetFunction);
 
-        function resetFunction() {
-
-            fetch("https://wt.ops.labs.vu.nl/api23/99a18706/reset", {
-                method: 'GET',
+    function resetFunction() {
+        fetch("https://wt.ops.labs.vu.nl/api23/99a18706/reset",{
+            method: 'GET',
+        })
+        .then(data=>{ console.log(data); 
+        })
+        .then(function(){
+            let nbRows = document.querySelectorAll('tr').length;
+            let table = document.getElementById("table");
+            for(var i = 1; i < nbRows-1; i++){
+                table.deleteRow(1);
+            }
+            authors.length = 0;
+            document.getElementById("sourcesList").innerHTML = "";
+            let idclear = document.getElementById("id");
+            idclear.innerHTML = '<option value="please select">Please Select</option>';
+        })
+        .then(function(){
+            fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{})
+            .then(function(response){
+                return response.json();
             })
-            .then(data=>{ console.log(data); 
+            .then(function(data){
+                data.forEach(function(person){
+                    if(isInVector(person.author) == false){
+                        authors.push(person.author);
+                        const insert = '<p>'+person.author+'</p>'
+                        let list = document.getElementById("sourcesList");
+                        list.insertAdjacentHTML("afterbegin", insert);
+                    }
+                    const markup = '<tr><td><div><img src = '+person.image+' alt="Picture of '+person.author+'"></div></td><td><button class="nameButton">'+person.author+'</button></td><td><p>'+person.alt+'</p></td><td><p>'+person.tags+'</p></td><td><p><b>This is a photo of '+person.author+'</p><p>'+person.description+'</p></td><td><p>'+person.id+'</p></td></tr>';
+                    let hahaKitten = document.getElementsByTagName("tr")[0];
+                    hahaKitten.insertAdjacentHTML('afterend', markup);
+
+                })
             })
             .then(function(){
-                let nbRows = document.querySelectorAll('tr').length;
-                let table = document.getElementById("table")
-                for(var i = 1; i < nbRows-1; i++){
-                    table.deleteRow(1);
-                }
-                authors.length = 0;
-                document.getElementById("sourcesList").innerHTML = "";
-            })
-            .then(function(){
-                fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{})
-                .then(function(response){
-                    return response.json();
-                })
-                .then(function(data){
-                    data.forEach(function(person){
-                        if(isInVector(person.author) == false){
-                            authors.push(person.author);
-                            const insert = '<p>'+person.author+'</p>'
-                            let list = document.getElementById("sourcesList");
-                            list.insertAdjacentHTML("afterbegin", insert);
-                        }
-                        const markup = '<tr><td><div><img src = '+person.image+' alt="Picture of '+person.author+'"></div></td><td><button class="nameButton">'+person.author+'</button></td><td><p>'+person.alt+'</p></td><td><p>'+person.tags+'</p></td><td><p><b>This is a photo of '+person.author+'</p><p>'+person.description+'</p></td><td><p>'+person.id+'</p></td></tr>';
-                        let hahaKitten = document.getElementsByTagName("tr")[0];
-                        hahaKitten.insertAdjacentHTML('afterend', markup);
-
-                    });
-                })
-                .then(function(){
-                    nameButtons = document.getElementsByClassName("nameButton");
-                    for (var i = 0; i < nameButtons.length; i++) {
-                        let btn = nameButtons[i]
-                        btn.addEventListener('click', function(){
-                            var filter, table, tr, td, i, txtValue;
-                            filter = btn.textContent;
-                            table = document.getElementById("table");
-                            tr = table.getElementsByTagName("tr");
-                            tr.length -= 2;
-                            filterModal.style.display = "block";
-                    
-                            for(i = 0; i < tr.length; i++){
-                                td = tr[i].getElementsByTagName("td")[1];
-                                if(td){
-                                    txtValue = td.textContent || td.innerText;
-                                    if(txtValue == filter){
-                                        tr[i].style.display = "";
-                                    }else{
-                                        tr[i].style.display = "none";
-                                    }
+                nameButtons = document.getElementsByClassName("nameButton");
+                for (var i = 0; i < nameButtons.length; i++) {
+                    let btn = nameButtons[i];
+                    btn.addEventListener('click', function(){
+                        var filter, table, tr, td, i, txtValue;
+                        filter = btn.textContent;
+                        table = document.getElementById("table");
+                        tr = table.getElementsByTagName("tr");
+                        tr.length -= 2;
+                        filterModal.style.display = "block";
+                
+                        for(i = 0; i < tr.length; i++){
+                            td = tr[i].getElementsByTagName("td")[1];
+                            if(td){
+                                txtValue = td.textContent || td.innerText;
+                                if(txtValue == filter){
+                                    tr[i].style.display = "";
+                                }else{
+                                    tr[i].style.display = "none";
                                 }
                             }
-                            who = document.getElementById("whoFilter");
-                            who.innerText = "Now filtering by: "+filter;
-                        });
-                    }
-                })
+                        }
+                        who = document.getElementById("whoFilter");
+                        who.innerText = "Now filtering by: "+filter;
+                    })
+                }
             })
+        })
 
-        }
+    }
 
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("modalButton");
     var span = document.getElementsByClassName("close")[0];
     var span2 = document.getElementsByClassName("close")[1];
-    
     var span3 = document.getElementsByClassName("close")[2];
 
     span3.addEventListener("click", function(){
@@ -255,8 +254,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     })
 
-    
-
     filterbar.addEventListener("keydown", function(){
         var input, filter, table, tr, td, i, txtValue;
         input = document.getElementById("filterBar");
@@ -277,13 +274,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 }
             }
         }
-
     })
-
-    /*For the filterable gallery functionality we will be using a modified implementation of the search bar from this site: https://www.w3schools.com/howto/howto_js_filter_table.asp */
 
     let el = document.getElementById('updateModal');
     let updateButton = document.getElementById("uButton");
+
     updateButton.addEventListener('click', function(){
         let selectBox = document.getElementById("id");
         rows = document.getElementsByTagName('tr');
@@ -293,7 +288,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         for(i = 0; i < rows.length; i++){
             td = rows[i].getElementsByTagName("td")[5];
             if(td){
-                ids.push(td.innerText)
+                ids.push(td.innerText);
             }
         }
 
@@ -302,39 +297,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
             selectBox.insertAdjacentHTML('beforeend', markup);
         }
         el.style.display="block";
-
     })
 
     span2.addEventListener("click", function(){
         el.style.display = "none";
+        let nbRows = document.querySelectorAll('tr').length;
+        let table = document.getElementById("table");
 
-            let nbRows = document.querySelectorAll('tr').length;
-            let table = document.getElementById("table")
-            for(var i = 1; i < nbRows-1; i++){
-                table.deleteRow(1);
-            }
-            authors.length = 0;
-            document.getElementById("sourcesList").innerHTML = "";
-        
-            fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{})
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(data){
-                data.forEach(function(person){
-                    if(isInVector(person.author) == false){
-                        authors.push(person.author);
-                        const insert = '<p>'+person.author+'</p>'
-                        let list = document.getElementById("sourcesList");
-                        list.insertAdjacentHTML("afterbegin", insert);
-                    }
-                    const markup = '<tr><td><div><img src = '+person.image+' alt="Picture of '+person.author+'"></div></td><td><button class="nameButton">'+person.author+'</button></td><td><p>'+person.alt+'</p></td><td><p>'+person.tags+'</p></td><td><p><b>This is a photo of '+person.author+'</p><p>'+person.description+'</p></td><td><p>'+person.id+'</p></td></tr>';
-                    let hahaKitten = document.getElementsByTagName("tr")[0];
-                    hahaKitten.insertAdjacentHTML('afterend', markup);
+        for(var i = 1; i < nbRows-1; i++){
+            table.deleteRow(1);
+        }
 
-                })
+        authors.length = 0;
+        document.getElementById("sourcesList").innerHTML = "";
+    
+        fetch("https://wt.ops.labs.vu.nl/api23/99a18706",{})
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            data.forEach(function(person){
+                if(isInVector(person.author) == false){
+                    authors.push(person.author);
+                    const insert = '<p>'+person.author+'</p>'
+                    let list = document.getElementById("sourcesList");
+                    list.insertAdjacentHTML("afterbegin", insert);
+                }
+                const markup = '<tr><td><div><img src = '+person.image+' alt="Picture of '+person.author+'"></div></td><td><button class="nameButton">'+person.author+'</button></td><td><p>'+person.alt+'</p></td><td><p>'+person.tags+'</p></td><td><p><b>This is a photo of '+person.author+'</p><p>'+person.description+'</p></td><td><p>'+person.id+'</p></td></tr>';
+                let hahaKitten = document.getElementsByTagName("tr")[0];
+                hahaKitten.insertAdjacentHTML('afterend', markup);
             })
-        
+        })
     })
 
     let box = document.getElementById("id");
@@ -346,23 +339,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
             data = rows[index].getElementsByTagName("td")[5]; 
             if(data){
                 if(data.textContent == selected){
-                    let bomba = document.getElementById("image1")
+                    let bomba = document.getElementById("image1");
                     let image = rows[index].getElementsByTagName("img")[0].getAttribute("src");
                     bomba.value = image;
 
-                    let bimba = document.getElementById("author1")
+                    let bimba = document.getElementById("author1");
                     let author = rows[index].getElementsByTagName("button")[0].innerText;
                     bimba.value = author;
 
-                    let bumba = document.getElementById("alt1")
+                    let bumba = document.getElementById("alt1");
                     let alt = rows[index].getElementsByTagName("p")[0].innerText;
                     bumba.value = alt;
 
-                    let bamba = document.getElementById("tags1")
+                    let bamba = document.getElementById("tags1");
                     let tag = rows[index].getElementsByTagName("p")[1].innerText;
                     bamba.value = tag;
 
-                    let bymba = document.getElementById("description1")
+                    let bymba = document.getElementById("description1");
                     let description = rows[index].getElementsByTagName("p")[2].innerText;
                     bymba.value = description;
                 }
@@ -372,35 +365,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     let updateSubmit = document.getElementById("update");
     updateSubmit.addEventListener("click", function(e){
-        
-            e.preventDefault();
-            console.log("poopy");
-            var imageInput = document.getElementById('image1').value
-            var authorInput = document.getElementById('author1').value
-            var altInput = document.getElementById('alt1').value
-            var tagsInput = document.getElementById('tags1').value
-            var descriptionInput = document.getElementById('description1').value
-            var imageId = document.getElementById("id").value
-            let str = "https://wt.ops.labs.vu.nl/api23/99a18706/item/"+imageId
-            console.log(str);
-    
-            fetch(str, {
-                method: 'PUT',
-                headers:{
-                    "Content-Type": "application/json; charset=UTF-8"
-                },
-                body:JSON.stringify({
-                    image: imageInput,
-                    author: authorInput,
-                    alt: altInput,
-                    tags: tagsInput,
-                    description: descriptionInput,
-                }),
+        e.preventDefault();
+        var imageInput = document.getElementById('image1').value;
+        var authorInput = document.getElementById('author1').value;
+        var altInput = document.getElementById('alt1').value;
+        var tagsInput = document.getElementById('tags1').value;
+        var descriptionInput = document.getElementById('description1').value;
+        var imageId = document.getElementById("id").value;
+        let str = "https://wt.ops.labs.vu.nl/api23/99a18706/item/"+imageId;
+        console.log(str);
+
+        fetch(str, {
+            method: 'PUT',
+            headers:{
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            body:JSON.stringify({
+                image: imageInput,
+                author: authorInput,
+                alt: altInput,
+                tags: tagsInput,
+                description: descriptionInput,
             })
-      
+        })
+        let idclear = document.getElementById("id");
+        idclear.innerHTML = '<option value="please select">Please Select</option>';
     })
-
-
-});
-
-
+})
